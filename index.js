e0621ef5730b23e61fd09b225ffe1cf9ff08a1f1
Sergio Berdeja <sergioberdeja@gmail.com>
2022-03-28 09:35:23 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array) {
+    let winObj = array.find(x => x.result === "W")
+    if (typeof winObj === 'undefined') {
+        return
+    } 
+    return winObj.year
+}
